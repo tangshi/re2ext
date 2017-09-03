e129103f48228d8810d91a3fe28d603de3fcdac0
tangshi <tangshi@t.shu.edu.cn>
2017-09-03 18:40:18 +0800

    add doc commenting

--- re2ext.h
+++ re2ext.h
@@ -9,15 +9,34 @@
 
 namespace re2 {
 
-MatchObject match(const std::string &string, re2::RE2 &pattern, bool detail=false, size_t offset=0);
+/** match `text` at beginning against `pattern`
+ *
+ * @param detail specify weather the returned MatchObject should store detail info or not, if true
+ *        you can get the detail info of the match result by invoking MatchObject methods like
+ *        MatchObject::group MatchObject::string MatchObject::start MatchObject::end etc.
+ * @param offset specify the beginning of the `text` to be matched. usually it's **0**
+ */
+MatchObject match(const std::string &text, re2::RE2 &pattern, bool detail=false, size_t offset=0);
 
-MatchObject search(const std::string &string, re2::RE2 &pattern, bool detail=false, size_t offset=0);
+MatchObject search(const std::string &text, re2::RE2 &pattern, bool detail=false, size_t offset=0);
 
-std::vector<std::string> findall(const std::string &string, re2::RE2 &pattern, size_t offset=0);
+/** find all non-overlapping substrings in the `text` that match the `pattern`
+ *
+ * @return a vector of matched substrings
+ */
+std::vector<std::string> findall(const std::string &text, re2::RE2 &pattern, size_t offset=0);
 
-std::string sub(const std::string &string, re2::RE2 &pattern, const std::string repl);
+/** replacing the leftmost non-overlapping occurrences
+ *  of the pattern in `text` by the replacement string `repl`
+ *
+ * @return the string obtained after replacing
+ */
+std::string sub(const std::string &text, re2::RE2 &pattern, const std::string repl);
 
-std::string sub(const std::string &string, re2::RE2 &pattern, std::function<std::string(MatchObject&)> repl);
+/** overload of sub function, except replacing occurrences of the pattern
+ *  by the the result of the callable function parameter `repl`
+ */
+std::string sub(const std::string &text, re2::RE2 &pattern, std::function<std::string(MatchObject&)> repl);
 
 }
 
